378e5bef56415ee8e09dc8b2147e8fb10d251f1b
suliana <sulieyana@gmail.com>
2025-07-23 11:44:09 +0800

    no message

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -232,10 +232,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 
 # Default storage settings, with the staticfiles storage updated.
 # See https://docs.djangoproject.com/en/5.2/ref/settings/#std-setting-STORAGES
